d893d6e827de60154a5b252ae23bca26acd252f8
Hassan Abdinur <hassanabdinur@Hassans-MacBook-Pro.local>
2019-09-13 01:59:06 -0700

    adding ingredients

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState("chicken");
 
 useEffect(() => {
   getRecipes();
-}, []);
+}, [query]);
 
 const getRecipes = async () => {
   const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
@@ -23,17 +23,17 @@ const getRecipes = async () => {
 
   const updateSearch = e => {
     setSearch(e.target.value);
-    console.log(search);
   }
 
   const getSearch = e => {
     e.preventDefault();
-    setQuery()
+    setQuery(search);
+    setSearch('');
   }
 
   return(
     <div className="App">
-    <form className="search-form">
+    <form onSubmit={getSearch} className="search-form">
       <input className="search-bar" type="text" value={search} onChange={updateSearch} />
       <button className="search-button" type="submit">
         Search
@@ -45,6 +45,7 @@ const getRecipes = async () => {
       title={recipe.recipe.label} 
       calories={recipe.recipe.calories}
       image={recipe.recipe.image}
+      ingredients={recipe.recipe.ingredients}
       />
     ))}
     </div>
