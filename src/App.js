937c6685a3e74235a81752b122bdb7c56c3574a9
Hassan Abdinur <hassanabdinur@Hassans-MacBook-Pro.local>
2019-09-13 01:07:16 -0700

    fetching api

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ const App = () => {
   const APP_ID = `${process.env.REACT_APP_RECIPE_API_ID}`
   const APP_KEY = `${process.env.REACT_APP_RECIPE_API_KEY}`
 
-useEffect(() => {
+  const [recipes, setRecipes] = useState([]);
 
+useEffect(() => {
+  getRecipes();
 }, []);
 
 const getRecipes = async () => {
   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data = await response.json();
-  console.log(data);
+  console.log(data.hits);
 }
 
   return(
