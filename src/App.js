a24501809dffcf29b1c4355208e7da2e88fac647
Hassan Abdinur <hassanabdinur@Hassans-MacBook-Pro.local>
2019-09-13 01:00:40 -0700

    working on fetching api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { async } from 'q';
 
 const App = () => {
-
   const APP_ID = `${process.env.REACT_APP_RECIPE_API_ID}`
   const APP_KEY = `${process.env.REACT_APP_RECIPE_API_KEY}`
 
-  const exampleReq =
-   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+useEffect(() => {
+
+}, []);
+
+const getRecipes = async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+  console.log(data);
+}
 
   return(
     <div className="App">
-    <h1>Hello React</h1>
+    <form className="search-form">
+      <input className="search-bar" type="text"/>
+      <button className="search-button" type="submit">
+        Search
+      </button>
+    </form>
     </div>
   );
 }
