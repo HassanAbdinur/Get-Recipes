39fdfc5354227012be49fa2478be0ef053db2036
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-04 19:35:08 -0700

    making signin route

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,10 +1,52 @@
 import React from 'react';
 
-const Signin = () => {
-    return (
-        
-    );
-}
+class Signin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
+    submitSignin(e) {}
+
+    render() {
+        return (
+            <div className="inner-container">
+                <div className="header">
+                    Signin
+                </div>
+                <div className="box">
+
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input 
+                        type="text" 
+                        name="username" 
+                        className="login-input" 
+                        placeholder="Username"
+                        />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        type="text" 
+                        name="password" 
+                        className="input-type" 
+                        placeholder="Password" 
+                        />
+                    </div>
+
+                    <button 
+                    type="button" 
+                    className="signin-btn" 
+                    onClick={this
+                    .submitSignin
+                    .bind(this)}>Signin</button>
+
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Signin;
