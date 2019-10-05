3b378d08cff7187c0d55daa45e02a387658fad73
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-04 20:10:00 -0700

    working on registration

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -16,8 +16,23 @@ class Register extends React.Component {
                 </div>
                 <div className="box">
                     <div className="input-group">
-                        
+                        <label htmlFor="username">Username</label>
+                        <input 
+                        type="text" 
+                        name="username" 
+                        className="login-input" 
+                        placeholder="Username"/>
                     </div>
+
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                        type="text" 
+                        name="email" 
+                        className="login-input" 
+                        placeholder="Email"/>
+                    </div>
+
                 </div>
             </div>
         );
