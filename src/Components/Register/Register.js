aad1243bdb38a9b15c98363e2ebb3af719a1f6d6
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-04 19:34:30 -0700

    creating registration form

---
+++ src/Components/Register/Register.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    submitRegister(e) {} 
+
+    render() {
+        return (
+            <div className="inner-container">
+                <div className="header">
+                    Register
+                </div>
+                <div className="box">
+                    <div className="input-group">
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
